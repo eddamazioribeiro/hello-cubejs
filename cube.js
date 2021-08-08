162bdbebf3bbfa7d98484c3c3e8a92cec5d8d920
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2021-08-08 11:42:46 -0300

    added logger

--- cube.js
+++ cube.js
@@ -1,3 +1,6 @@
 // Cube.js configuration options: https://cube.dev/docs/config
 module.exports = {
+  logger: (msg, params) => {
+    console.log(`[logger] ${msg}: ${JSON.stringify(params)}`);
+  }
 };
